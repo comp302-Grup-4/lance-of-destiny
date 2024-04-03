0282add8909dca48ace5e6465b22a48d9487918a
cyuksel21 <cyuksel21@ku.edu.tr>
2024-04-03 22:57:38 +0300

    spellDepot methods are added

--- src/domain/animation/SpellDepot.java
+++ src/domain/animation/SpellDepot.java
@@ -1,5 +1,41 @@
 package domain.animation;
 
-public class SpellDepot {
+import java.util.HashMap;
 
+public class SpellDepot {
+	private HashMap<Spell, Integer> spellMap;
+	
+	public SpellDepot(HashMap<Spell, Integer> spellMap) {
+		this.spellMap = new HashMap<Spell,Integer>();
+	}
+
+	public HashMap<Spell, Integer> getSpellMap() {
+		return spellMap;
+	}
+
+	public void setSpellMap(HashMap<Spell, Integer> spellMap) {
+		this.spellMap = spellMap;
+	}
+	
+	public void addSpell(Spell s) {
+		if(!spellMap.containsKey(s)) {
+			spellMap.put(s, 1);
+		}
+		else {
+			spellMap.replace(s, spellMap.get(s) + 1);
+		}
+	}
+	
+	public void popSpell(Spell s) {
+		if (spellMap.get(s)==1) {
+			spellMap.remove(s);
+		}
+		else if (spellMap.get(s)>1) {
+			spellMap.replace(s, spellMap.get(s)-1);
+		}
+	}
+	
+	public boolean checkSpellExists(Spell s) {
+		return spellMap.containsKey(s);
+	}
 }
