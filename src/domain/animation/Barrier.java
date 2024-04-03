1e397c9fecf82024c69e01b2701d27c8a6d16633
Eren Karakuş <ekarakus20@ku.edu.tr>
2024-04-03 21:00:03 +0300

    barrier types added

--- src/domain/animation/Barrier.java
+++ src/domain/animation/Barrier.java
@@ -1,5 +1,10 @@
 package domain.animation;
 
 public abstract class Barrier implements Movable {
-
+	static final String SIMPLE_BARRIER = "simple";
+	static final String FIRM_BARRIER = "firm";
+	static final String EXPLOSIVE_BARRIER = "explosive";
+	static final String GIFT_BARRIER = "gift";
+	
+	
 }
