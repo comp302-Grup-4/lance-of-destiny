fc8a8c72f8e032f2fed83ea1cfb7a54cc36965d1
yazralkan <115217813+yazralkan@users.noreply.github.com>
2024-04-03 22:46:14 +0300

    player account initial methods and attributes

--- src/domain/PlayerAccount.java
+++ src/domain/PlayerAccount.java
@@ -1,5 +1,40 @@
 package domain;
 
-public class PlayerAccount {
+import java.util.List;
 
+public class PlayerAccount {
+	private String userName;
+	private String userPassword;
+	private List <Integer> highScores;
+	
+	public PlayerAccount(String userName, String userPassword, List<Integer> highScores) {
+		super();
+		this.userName = userName;
+		this.userPassword = userPassword;
+		this.highScores = highScores;
+	}
+	
+	public void addNewScore(Integer newScore) {
+		highScores.add(newScore);
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	
+	public void associateGame(Game game) {
+		
+	}
 }
