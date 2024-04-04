b61f4dd9a10f71377c531fe09b43411add849673
Eren Karakuş <ekarakus20@ku.edu.tr>
2024-04-04 18:56:39 +0300

    fireball and magical staff basics implemented

--- src/domain/animation/FireBall.java
+++ src/domain/animation/FireBall.java
@@ -1,5 +1,39 @@
 package domain.animation;
 
 public class FireBall implements Movable {
+	private Vector position;
+	private Vector velocity;
+	
+	public FireBall(int posX, int posY) {
+		position = new Vector(posX, posY);
+		velocity = Vector.fromDegrees(90).scale(10);
+	}
+
+	@Override
+	public Vector getNextPosition(float dtime) {
+		return position.add(velocity.scale(dtime));
+	}
+
+	@Override
+	public Vector move(float dtime) {
+		this.position = getNextPosition(dtime);
+		return this.position;
+	}
+
+	public Vector getPosition() {
+		return position;
+	}
+
+	public void setPosition(Vector position) {
+		this.position = position;
+	}
+
+	public Vector getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(Vector velocity) {
+		this.velocity = velocity;
+	}
 
 }
--- src/domain/animation/MagicalStaff.java
+++ src/domain/animation/MagicalStaff.java
@@ -1,5 +1,30 @@
 package domain.animation;
 
 public class MagicalStaff implements Movable {
+	private Vector position;
+	private Vector velocity;
+	public static int MS_HORIZON = 950; // MS will not move up and down 
+	
+	public MagicalStaff() {
+		position = new Vector(500, MS_HORIZON);
+		velocity = new Vector(0, 0);
+	}
+	
+	@Override
+	public Vector getNextPosition(float dtime) {
+		position = position.add(velocity.scale(dtime));
+		return position;
+	}
+
+	@Override
+	public Vector move(float dtime) {
+		this.position = getNextPosition(dtime);
+		return this.position;
+	}
+
+	@Override
+	public void setVelocity(Vector newVelocity) {
+		this.velocity = newVelocity;
+	}
 
 }
