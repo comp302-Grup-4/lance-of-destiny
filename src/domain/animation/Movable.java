77cc292882149383e950922d237e04972a02613e
Eren Karakuş <ekarakus20@ku.edu.tr>
2024-04-08 12:34:02 +0300

    getRotation and getPosition at Movable interface

--- src/domain/animation/Movable.java
+++ src/domain/animation/Movable.java
@@ -1,6 +1,8 @@
 package domain.animation;
 
 public interface Movable {
+	Vector getPosition();
+	float getRotation();
 	Vector getNextPosition(float dtime);
 	Vector move(float dtime);
 	void setVelocity(Vector newVelocity);
