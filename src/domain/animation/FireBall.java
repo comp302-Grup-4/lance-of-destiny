7dea7ba29e75554f97d46b2ceecd5e2e9df75caf
Eren Karakuş <ekarakus20@ku.edu.tr>
2024-04-09 05:49:17 +0300

    fireball size and initial position added

--- src/domain/animation/FireBall.java
+++ src/domain/animation/FireBall.java
@@ -3,13 +3,15 @@ package domain.animation;
 public class FireBall extends AnimationObject {
 	
 	public FireBall() {
-		this(500, 950);
+		this(492, 600);
 	}
 	
 	public FireBall(int posX, int posY) {
 		super();
 		position = new Vector(posX, posY);
-		velocity = Vector.fromDegrees(90).scale(10);
+		velocity = Vector.fromDegrees(90).scale(400);
+		sizeX = 16;
+		sizeY = 16;
 	}
 	
 	@Override
