1c2bce82301a1170edad175e96e34f2b6ef5c386
Eren Karakuş <ekarakus20@ku.edu.tr>
2024-04-09 21:09:38 +0300

    runningScreen with only playView for testing

--- src/ui/RunningScreen.java
+++ src/ui/RunningScreen.java
@@ -1,16 +1,35 @@
 package ui;
 
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import javax.swing.JPanel;
 
-public class RunningScreen extends JPanel {
+import domain.Game;
+import ui.playview.PlayView;
 
+public class RunningScreen extends JPanel {	
 	private static final long serialVersionUID = 5L;
-
+	private PlayView playView;
 	/**
 	 * Create the panel.
 	 */
-	public RunningScreen() {
+	public RunningScreen(Game game) {
+		playView = new PlayView(this, game);
+		game.getAnimator();
+		
+		this.setLayout(new GridBagLayout());
+		
+		GridBagConstraints gbc = new GridBagConstraints();
+		
+		gbc.fill = GridBagConstraints.BOTH;
+		gbc.gridx = 0;
+		gbc.gridy = 0;
+		gbc.weightx = 1;
+		gbc.weighty = 1;
 
+		playView.setFocusable(true);
+		playView.setVisible(true);
+		
+		this.add(playView, gbc);
 	}
-
 }
